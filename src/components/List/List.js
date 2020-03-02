ec81839e2a73e78e2881fca9a1e7bba14402f3c0
Adam Goss <adam.goss94@gmail.com>
2020-03-02 17:30:53 +0100

    Add creatorjs and button.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,12 +5,29 @@ import Column from '../Column/Column.js';
 import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore.js';
 import ReactHtmlParser from 'react-html-parser';
+import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
   state = {
     columns: this.props.columns || [],
   }
 
+  addColumn(title){
+    this.setState(state => (
+      {
+        columns: [
+          ...state.columns,
+          {
+            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
+            title,
+            icon: 'list-alt',
+            cards: []
+          }
+        ]
+      }
+    ));
+  }
+  
   render() {
     return (
       <section className={styles.component}>
@@ -25,6 +42,11 @@ class List extends React.Component {
             <Column key={key} {...columnProps} />
           ))}
         </div>
+
+        <div className={styles.creator}>
+          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
+        </div>
+
       </section>
     )
   }
