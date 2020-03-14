e3f1e5098f5d03b08b6c6100464c3ff2ab4baf52
Adam Goss <adam.goss94@gmail.com>
2020-03-14 13:31:18 +0100

    Add redux and refactor App

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,43 +1,24 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
-import Column from '../Column/Column.js';
 import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore.js';
 import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator.js';
+/* import Creator from '../Creator/Creator.js';
+import Column from '../Column/Column.js'; */
 
 class List extends React.Component {
-  state = {
-    columns: this.props.columns || [],
-  }  
-
-  addColumn(title){
-    this.setState(state => (
-      {
-        columns: [
-          ...state.columns,
-          {
-            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
-            title,
-            icon: 'list-alt',
-            cards: [],
-          },
-        ],
-      }
-    ));
-  }
-  
   render() {
+    const {title, image, description} = this.props;
+
     return (
       <section className={styles.component}>
-        <Hero titleText={this.props.title} imageSource={this.props.image}/>
-
+        <Hero titleText={title} image={image} />
         <div className={styles.description}>
-          {ReactHtmlParser(this.props.description)}
+          {ReactHtmlParser(description)}
         </div>
 
-        <div className={styles.columns}>
+        {/* <div className={styles.columns}>
           {this.state.columns.map(({key, ...columnProps}) => (
             <Column key={key} {...columnProps} />
           ))}
@@ -45,7 +26,8 @@ class List extends React.Component {
 
         <div className={styles.creator}>
           <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
-        </div>
+        </div> */
+        }
 
       </section>
     );
