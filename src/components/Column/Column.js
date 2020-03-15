8839135ea30fdc818c742aa051951d2095c70bd1
Adam Goss <adam.goss94@gmail.com>
2020-03-15 17:32:03 +0100

    Add functionality responsible for adding cards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-import {settings} from '../../data/dataStore.js';
-/* import Creator from '../Creator/Creator.js'; */
+import {settings} from '../../data/dataStore';
+import Creator from '../Creator/Creator.js';
 import Card from '../Card/Card';
 import Icon from '../Icon/Icon';
 
 class Column extends React.Component { 
   render() {
-    const {title, icon, cards} = this.props;
+    const {title, icon, cards, addCard} = this.props;
     
     return (
       <section className={styles.component}>
@@ -25,9 +25,9 @@ class Column extends React.Component {
           ))}
         </div>
 
-        {/* <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-        </div> */}
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={addCard}/>
+        </div>
 
       </section>
     );
@@ -37,6 +37,7 @@ class Column extends React.Component {
     title: PropTypes.string.isRequired,
     icon: PropTypes.string,
     cards: PropTypes.array,
+    addCard: PropTypes.func,
   }
 
   static defaultProps = {
