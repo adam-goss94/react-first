cb1ce1eae2c1958b225c4f334e892ec06eee207a
Adam Goss <adam.goss94@gmail.com>
2020-03-19 18:23:23 +0100

    Add Container component and use it in List and Search

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,35 +1,36 @@
 import React from 'react';
 import styles from './List.scss';
-import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
-import {settings} from '../../data/dataStore.js';
 import ReactHtmlParser from 'react-html-parser';
+import {settings} from '../../data/dataStore';
+import Hero from '../Hero/Hero';
 import Creator from '../Creator/Creator.js';
-import Column from '../Column/ColumnContainer.js';
+import Column from '../Column/ColumnContainer';
+import Container from '../Container/Container';
 
 class List extends React.Component {
   render() {
     const {title, image, description, columns, addColumn} = this.props;
     
     return (
-      <section className={styles.component}>
-        <Hero titleText={title} image={image} />
-        <div className={styles.description}>
-          {ReactHtmlParser(description)}
-        </div>
+      <Container>
+        <section className={styles.component}>
+          <Hero titleText={title} image={image} />
+          <div className={styles.description}>
+            {ReactHtmlParser(description)}
+          </div>
 
-        <div className={styles.columns}>
-          {columns.map(columnData => (
-            <Column key={columnData.id} {...columnData} />
-          ))}
-        </div>
+          <div className={styles.columns}>
+            {columns.map(columnData => (
+              <Column key={columnData.id} {...columnData} />
+            ))}
+          </div>
 
-        <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={addColumn}/>
-        </div>
-        
-
-      </section>
+          <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={addColumn}/>
+          </div>
+        </section>
+      </Container>
     );
   }
   
